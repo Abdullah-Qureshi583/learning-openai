6b56ceb52b8c286a5049d4e3056a4e7cef9f8e6a
Abdullah Qureshi <mabdullahqureshi583@gmail.com>
2025-07-26 16:14:37 +0500

    update syncronuous way

---
+++ code/sync_way.py
@@ -0,0 +1,25 @@
+from agents import Agent, Runner, OpenAIChatCompletionsModel
+import asyncio
+import os 
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+MODEL = "gemini-2.0-flash"
+client = AsyncOpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model=MODEL,
+    openai_client=client
+)
+
+agent = Agent(name="Assistant", instructions="You are a helpful assistant", model=model)
+
+result = Runner.run_sync(agent, "Write a haiku about recursion in programming.")
+print(result.final_output)
