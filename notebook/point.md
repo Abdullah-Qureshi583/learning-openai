30dcc3aa6c2dbbc71263ef3ba5bae8417ed2ed21
Abdullah Qureshi <mabdullahqureshi583@gmail.com>
2025-08-05 10:13:13 +0500

    add points

--- notebook/point.md
+++ notebook/point.md
@@ -1,9 +1,13 @@
 ```python
 1.The agent just requires the name but don't require the instructions
 
-1.It is necessary to marked the function as tool with decorator
+2.When using the runner as sync so it is necessary to use without in async function otherwise will get an error
 
-2.audioitransaction api -> use for voice to text conversion ->	/v1/audio/transcriptions
+3.When using the runner as async so it is necessary to use it in the  async function otherwise will get an error
 
-3. Always use run and await when working with asyncronously like use asyncio
+4.It is necessary to marked the function as tool with decorator
+
+5.audioitransaction api -> use for voice to text conversion ->	/v1/audio/transcriptions
+
+6. Always use run and await when working with asyncronously like use asyncio
 ```
--- practice.py
+++ practice.py
@@ -1,11 +1,129 @@
-from agents import Agent, Runner, OpenAIChatCompletionsModel, function_tool
-import asyncio
-from dataclasses import dataclass
-from pydantic import BaseModel
-import os 
-from dotenv import load_dotenv
+# from agents import Agent, Runner, OpenAIChatCompletionsModel, function_tool
+# import asyncio
+# from dataclasses import dataclass
+# from pydantic import BaseModel
+# import os 
+# from dotenv import load_dotenv
+# from openai import AsyncOpenAI
+# from agents.run import RunConfig
+# load_dotenv()
+
+# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+# MODEL = "gemini-2.0-flash"
+
+# client = AsyncOpenAI(
+#     api_key=GEMINI_API_KEY,
+#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+# )
+
+# model = OpenAIChatCompletionsModel(
+#     model=MODEL,
+#     openai_client=client
+# )
+
+# config = RunConfig(
+#     model=model,
+#     model_provider=client,
+#     tracing_disabled=True
+# )
+
+# # # history_tutor_agent = Agent(
+# # #     name="History Tutor",
+# # #     handoff_description="Specialist agent for historical questions",
+# # #     instructions="You provide assistance with historical queries. Explain important events and context clearly.",
+# # # )
+
+# # # math_tutor_agent = Agent(
+# # #     name="Math Tutor",
+# # #     handoff_description="Specialist agent for math questions",
+# # #     instructions="You provide help with math problems. Explain your reasoning at each step and include examples",
+# # # )
+
+
+# # # triage_agent = Agent(
+# # #     name="Triage Agent",
+# # #     instructions="You determine which agent to use based on the user's homework question",
+# # #     handoffs=[history_tutor_agent, math_tutor_agent]
+# # # )
+
+# # # @function_tool
+# # # def get_weather(city: str) -> str:
+# # #     """returns weather info for the specified city."""
+# # #     return f"The weather in {city} is sunny"
+
+
+# # # agent = Agent(
+# # #     name="Haiku agent",
+# # #     instructions="Always respond in haiku form",
+# # #     tools=[get_weather],
+# # # )
+
+
+# # # class CalendarEvent(BaseModel):
+# # #     name: str
+# # #     date: str
+# # #     participants: list[str]
+
+# # # agent = Agent(
+# # #     name="Calendar extractor",
+# # #     instructions="Extract calendar events from text",
+# # #     output_type=CalendarEvent,
+# # # )
+
+# # # result = Runner.run_sync(agent, "at the day on 21 feb, abdullah qureshi was born and joined by nasir and ali",  run_config=config);
+# # # print(result);
+
+
+
+# @dataclass
+# class UserContext:
+#     uid: str
+#     is_pro_user: bool
+#     def prev_item(self):
+#         return f"Previous item for {self.uid} are apple and banana"
+
+
+# def dynamic_instructions(context: UserContext, agent) -> str:
+#     print("Dynamic instructions called with context:", context)
+#     print("Dynamic instructions called with agent:", agent)
+#     if context.context.is_pro_user:
+#         return "You are helping a Pro user. Offer premium support and discounts."
+#     else:
+#         return "You are helping a Free user. Just offer standard support."
+
+
+
+
+# agent = Agent[UserContext](
+#     name="SupportAgent",
+#     instructions=dynamic_instructions,  # 👈 we pass the function, not static text
+# )
+
+
+
+# pro_user = UserContext(uid="abdullah001", is_pro_user=True)
+# free_user = UserContext(uid="guest123", is_pro_user=False)
+
+
+# response_pro = Runner.run_sync(agent,context=pro_user, run_config=config, input="am i a pro user? if yes, what are my previous items?")
+# print("🟢 Pro User Response:", response_pro)
+
+# # # Run for Free User
+# # response_free = Runner.run_sync(agent, context=free_user,run_config=config, input="I need help")
+# # print("🔵 Free User Response:", response_free)
+
+
+
+
+
+
+from agents import Agent, Runner, OpenAIChatCompletionsModel
 from openai import AsyncOpenAI
+from dotenv import load_dotenv
+import asyncio
+import os
 from agents.run import RunConfig
+
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
@@ -27,89 +145,18 @@ config = RunConfig(
     tracing_disabled=True
 )
 
-# # history_tutor_agent = Agent(
-# #     name="History Tutor",
-# #     handoff_description="Specialist agent for historical questions",
-# #     instructions="You provide assistance with historical queries. Explain important events and context clearly.",
-# # )
-
-# # math_tutor_agent = Agent(
-# #     name="Math Tutor",
-# #     handoff_description="Specialist agent for math questions",
-# #     instructions="You provide help with math problems. Explain your reasoning at each step and include examples",
-# # )
-
-
-# # triage_agent = Agent(
-# #     name="Triage Agent",
-# #     instructions="You determine which agent to use based on the user's homework question",
-# #     handoffs=[history_tutor_agent, math_tutor_agent]
-# # )
-
-# # @function_tool
-# # def get_weather(city: str) -> str:
-# #     """returns weather info for the specified city."""
-# #     return f"The weather in {city} is sunny"
-
-
-# # agent = Agent(
-# #     name="Haiku agent",
-# #     instructions="Always respond in haiku form",
-# #     tools=[get_weather],
-# # )
-
-
-# # class CalendarEvent(BaseModel):
-# #     name: str
-# #     date: str
-# #     participants: list[str]
-
-# # agent = Agent(
-# #     name="Calendar extractor",
-# #     instructions="Extract calendar events from text",
-# #     output_type=CalendarEvent,
-# # )
-
-# # result = Runner.run_sync(agent, "at the day on 21 feb, abdullah qureshi was born and joined by nasir and ali",  run_config=config);
-# # print(result);
-
-
-
-@dataclass
-class UserContext:
-    uid: str
-    is_pro_user: bool
-    def prev_item(self):
-        return f"Previous item for {self.uid} are apple and banana"
-
-
-def dynamic_instructions(context: UserContext, agent) -> str:
-    print("Dynamic instructions called with context:", context)
-    print("Dynamic instructions called with agent:", agent)
-    if context.context.is_pro_user:
-        return "You are helping a Pro user. Offer premium support and discounts."
-    else:
-        return "You are helping a Free user. Just offer standard support."
-
-
-
-
-agent = Agent[UserContext](
-    name="SupportAgent",
-    instructions=dynamic_instructions,  # 👈 we pass the function, not static text
+# async def main():
+agent = Agent(
+    name="Helpful Assistant",
+    instructions="You are a helpful assistant.",
+    
+    
 )
 
+user_input = str(input("How can I help you? "))
+result =  Runner.run_sync(agent, input=user_input, run_config=config)
 
+print("🟢Response:", result.final_output)
 
-pro_user = UserContext(uid="abdullah001", is_pro_user=True)
-free_user = UserContext(uid="guest123", is_pro_user=False)
-
-
-response_pro = Runner.run_sync(agent,context=pro_user, run_config=config, input="am i a pro user? if yes, what are my previous items?")
-print("🟢 Pro User Response:", response_pro)
-
-# # Run for Free User
-# response_free = Runner.run_sync(agent, context=free_user,run_config=config, input="I need help")
-# print("🔵 Free User Response:", response_free)
-
-
+# if __name__ == "__main__":
+    # asyncio.run(main())
