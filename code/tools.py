4ae7b9dba51612cda7219dad099fdd6c4f928ee6
Abdullah Qureshi <mabdullahqureshi583@gmail.com>
2025-08-19 00:12:38 +0500

    in tool add agent as a tool

--- code/tools.py
+++ code/tools.py
@@ -19,7 +19,7 @@ from agents.run import RunConfig
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-MODEL = "gemini-2.0-flash"
+MODEL = "gemini-2.5-flash"
 
 client = AsyncOpenAI(
     api_key=GEMINI_API_KEY,
@@ -38,7 +38,7 @@ config = RunConfig(
 )
 
 # function tool
-async def main():   
+async def function_tool_main():   
     
     @function_tool(name_override="get_languages",description_override="get the languages for the specific city")
     async def fetch_weather(city:str)-> str:
@@ -58,8 +58,65 @@ async def main():
     
     await run_demo_loop(agent)
 
+
+# function as a tool
+async def agent_as_tool():   
+        
+    spanish_agent = Agent(
+        name="Spanish agent",
+        instructions="You translate the user's message  just to Spanish",
+        model=model
+        
+    )
+
+    french_agent = Agent(
+        name="French agent",
+        instructions="You translate the user's message  just to French",
+        model=model
+        
+    )
+    
+    
+    orchestrator_agent = Agent(
+        name="orchestrator_agent",
+        instructions=(
+            "You are helpful assistant who reply the user queries."
+            # "If the user want to translate  so use the tools given to you to translate."
+            # "Don't translate until user ask to translate."
+            # "If asked for multiple translations, you call the relevant tools."
+            # "If the user ask about to translate and other task that you cannot, so translate and reply for the other tasks."
+            # # "If user ask for different languages instead of spanish and french so say don't know in a concise way"
+        ),
+        # tools=[
+        #     spanish_agent.as_tool(
+        #         tool_name="translate_to_spanish",
+        #         tool_description="Translate the user's message to Spanish",
+        #     ),
+        #     french_agent.as_tool(
+        #         tool_name="translate_to_french",
+        #         tool_description="Translate the user's message to French",
+        #     ),
+        # ],
+        model=model
+        
+        # handoffs=[french_agent,spanish_agent]
+    )
+                
+    while True:
+        user_input = str(input("How can I help you? "))
+        if user_input.strip().lower() in ["exit", "quit"]:
+            return
+        result = await Runner.run(orchestrator_agent, input=user_input)
+        print("🟢Response:", result)
+        
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    # asyncio.run(function_tool_main())
+    asyncio.run(agent_as_tool())
+
+
+
+# function tool
 
 
 
