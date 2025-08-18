fdcdc589073bbb2b26465945e1f3fb2fae04b9b1
Abdullah Qureshi <mabdullahqureshi583@gmail.com>
2025-08-18 14:09:48 +0500

    done learning and code about functino tool

--- code/tools.py
+++ code/tools.py
@@ -1,75 +1,65 @@
-# # there are 3 type of tools
-# # 1. hosted tools
-# # 2. Function Tool
-# # 3. Function as tool
+# there are 3 type of tools
+# 1. hosted tools
+# 2. Function Tool
+# 3. Function as tool
 
-# import json
-# from typing_extensions import TypedDict, Any
 
-# from agents import Agent, FunctionTool, RunContextWrapper, function_tool
+from typing_extensions import TypedDict, Any
 
-# from agents import Agent, Runner, OpenAIChatCompletionsModel
-# from openai import AsyncOpenAI
-# from dotenv import load_dotenv
-# import asyncio
-# import os
-# from agents.run import RunConfig
+from agents import Agent, FunctionTool, RunContextWrapper, function_tool, enable_verbose_stdout_logging,run_demo_loop
 
-# load_dotenv()
+from agents import Agent, Runner, OpenAIChatCompletionsModel
+from openai import AsyncOpenAI
+from dotenv import load_dotenv
+import asyncio
+import os
+from agents.run import RunConfig
 
-# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-# MODEL = "gemini-2.0-flash"
+# enable_verbose_stdout_logging()
+load_dotenv()
 
-# client = AsyncOpenAI(
-#     api_key=GEMINI_API_KEY,
-#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
-# )
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MODEL = "gemini-2.0-flash"
 
-# model = OpenAIChatCompletionsModel(
-#     model=MODEL,
-#     openai_client=client
-# )
+client = AsyncOpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
 
-# config = RunConfig(
-#     model=model,
-#     model_provider=client,
-#     tracing_disabled=True
-# )
+model = OpenAIChatCompletionsModel(
+    model=MODEL,
+    openai_client=client
+)
 
+config = RunConfig(
+    model=model,
+    model_provider=client,
+    tracing_disabled=True
+)
+
+# function tool
+async def main():   
     
-# @function_tool  
-# async def count_lines(file_name:str):
-#     with open("result.py") as f:
-#     print(f.read())
-
-# async def main():   
-
+    @function_tool(name_override="get_languages",description_override="get the languages for the specific city")
+    async def fetch_weather(city:str)-> str:
+        return f"The languages usually spoken in {city} are urdu and panjabi."
+        # return f"The weather in {city} is cloudy."
    
-#     agent = Agent(
-#         name="Assistant",
-#         tools=[count_lines],  
-#     )
+    agent = Agent(
+        name="Assistant",
+        tools=[fetch_weather],  
+        model=model
+    )
 
             
-#     user_input = str(input("How can I help you? "))
-#     result = await Runner.run(agent, input=user_input, run_config=config)
+    # user_input = str(input("How can I help you? "))
+    # result = await Runner.run(agent, input=user_input, run_config=config)
+    # print("🟢Response:", result.final_output)
+    
+    await run_demo_loop(agent)
 
-#     print("🟢Response:", result.final_output)
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
-# # context, dynamic instruction, prompt, session
-# # pdf summerizer
+if __name__ == "__main__":
+    asyncio.run(main())
 
 
 
-def count_lines(file_name:str):
-    with open("code/result.py", "r") as f:
-        # data = 'some data to be written to the file'
-        # f.write(data)
-        lines = f.readlines()
-        print(len(lines))
-        
-        
-count_lines("a")
